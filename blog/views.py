1956510dcf4c4a772d14326aceb0439e23b49538
Ric <ric@mail.ru>
2023-07-03 20:57:13 +0300

    final Django simple blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
+"""
+Post views
+"""
 from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
 
+
 def post_list(request):
+    """
+    post view
+    """
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
